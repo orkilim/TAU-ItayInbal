8f0b4829ba313ac248bc36451ff386bcd3656c63
or kilim <48524233+orkilim@users.noreply.github.com>
2021-09-10 13:34:04 +0300

    can receive the UIschema well on client side

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { JsonForms } from '@jsonforms/react';
-import { person } from '@jsonforms/examples';
 import Schema from './Components/SchemaComps/Schema.json'
 import UiSchema from './Components/SchemaComps/UiSchema.json'
 import {
   materialRenderers,
   materialCells,
 } from '@jsonforms/material-renderers';
-
-//const schema = person.schema;
-//const uischema = person.uischema;
-//const initialData = person.data;
+//import fs from 'fs'
+import axios from 'axios'
 
 const schema = Schema;
 const uischema = UiSchema;
 
+const axiosCall=()=>{
+  axios.post("http://localhost:3030/route/addForm/",
+    {
+      "form_title": "Itay and Inbal's project",
+      "1": {
+        "field_name": "is eligable for experiment",
+        "field_type": "boolean"
+      },
+      "2": {
+        "field_name": "age",
+        "field_type": "numeric"
+      }
+    })
+    .then((data) => {
+      const UI_schema=JSON.parse(data.config.data)
+      console.log(UI_schema)
+    })
+    .catch((error) => {
+      if (error)
+        console.log("that was the error:\n"+error)
+    }
+    )
+}
 
 function App() {
-  
+
   const [data, setData] = useState();
 
+  axiosCall()
+  
   return (
     <div className='App'>
       <JsonForms
