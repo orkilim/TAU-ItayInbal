22a6ce06da4967f09b8a2506ce03d386b7210d2b
or kilim <48524233+orkilim@users.noreply.github.com>
2021-09-12 17:00:42 +0300

    figure out how to create the forms with the jsons

--- src/App.js
+++ src/App.js
@@ -1,54 +1,62 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+//import schema from './Components/SchemaComps/schema.json'
+//import UI from './Components/SchemaComps/UI.json'
 import { JsonForms } from '@jsonforms/react';
-import Schema from './Components/SchemaComps/Schema.json'
-import UiSchema from './Components/SchemaComps/UiSchema.json'
+//import Schema from './Components/SchemaComps/Schema.json'
+//import UiSchema from './Components/SchemaComps/UiSchema.json'
 import {
   materialRenderers,
   materialCells,
 } from '@jsonforms/material-renderers';
 //import fs from 'fs'
 import axios from 'axios'
+import { Generate } from '@jsonforms/core';
 
-const schema = Schema;
-const uischema = UiSchema;
+//const schema = Schema;
+//const uischema = UiSchema;
 
-const axiosCall=()=>{
-  axios.post("http://localhost:3030/route/addForm/",
-    {
-      "form_title": "Itay and Inbal's project",
-      "1": {
-        "field_name": "is eligable for experiment",
-        "field_type": "boolean"
-      },
-      "2": {
-        "field_name": "age",
-        "field_type": "numeric"
-      }
-    })
-    .then((data) => {
-      const UI_schema=JSON.parse(data.config.data)
-      console.log(UI_schema)
-    })
-    .catch((error) => {
-      if (error)
-        console.log("that was the error:\n"+error)
-    }
-    )
-}
 
 function App() {
 
   const [data, setData] = useState();
+  const [UI, setUI] = useState({})
+  const [schema, setSchema] = useState({})
 
-  axiosCall()
-  
+  useEffect(()=>{
+    axios.post('http://localhost:3030/route/addForm/', {
+    "form_title": "Itay and Inbal's project",
+    "1": {
+      "field_name": "name",
+      "field_type": "string",
+      "minLength": 3,
+      "maxLength": 10
+    },
+    "2": {
+      "field_name": "age",
+      "field_type": "number",
+      "minimum": 18
+    }
+  })
+    .then((data) => {
+      setUI(data.data.UI)
+      setSchema(data.data.schema)
+    })
+    .catch((err) => {
+      if (err)
+        console.log("error is in App: " + err)
+    })
+  },[])
+
+  console.log(JSON.stringify(UI))
+  console.log(JSON.stringify(schema))
+
   return (
     <div className='App'>
       <JsonForms
         schema={schema}
-        uischema={uischema}
-        data={data}
+        uischema={UI}
+        //data={data}
         renderers={materialRenderers}
         cells={materialCells}
         onChange={({ data, _errors }) => setData(data)}
