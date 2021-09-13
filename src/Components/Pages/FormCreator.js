b44ae002125bf700d43134718b8a73e79f8cf54a
or kilim <48524233+orkilim@users.noreply.github.com>
2021-09-13 17:01:06 +0300

    starting to make the form creator...still

--- src/Components/Pages/FormCreator.js
+++ src/Components/Pages/FormCreator.js
@@ -1,49 +1,68 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import {addRequestedDataField} from '../SchemaComps/DataMethods'
 
 const FormCreator = () => {
     const [formValues, setFormValues] = useState([{ name: "", email : ""}])
+    const [dataFields,setDataFields]=useState([])
 
-    let handleChange = (i, e) => {
+    //#region Resercher's info methods
+    const handleChange = (i, e) => {
         let newFormValues = [...formValues];
         newFormValues[i][e.target.name] = e.target.value;
         setFormValues(newFormValues);
       }
     
-    let addFormFields = () => {
+    const addInfoFields = () => {
         setFormValues([...formValues, { name: "", email: "" }])
       }
     
-    let removeFormFields = (i) => {
+    const removeInfoFields = (i) => {
         let newFormValues = [...formValues];
         newFormValues.splice(i, 1);
         setFormValues(newFormValues)
     }
     
-    let handleSubmit = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
         alert(JSON.stringify(formValues));
     }
+    //#endregion
+    
+    //#region Data fields methods
+    const addDataFields = () => {
+        setFormValues([...formValues, addRequestedDataField()])
+      }
+    //#endregion
 
     return (
         <form  onSubmit={handleSubmit}>
           {formValues.map((element, index) => (
             <div className="form-inline" key={index}>
+            <h2>Please Enter Resercher's info:</h2>
               <label>Name</label>
               <input type="text" name="name" value={element.name || ""} onChange={e => handleChange(index, e)} />
               <label>Email</label>
               <input type="text" name="email" value={element.email || ""} onChange={e => handleChange(index, e)} />
               {
                 index ? 
-                  <button type="button"  className="button remove" onClick={() => removeFormFields(index)}>Remove</button> 
+                  <button type="button"  className="button remove" onClick={() => removeInfoFields(index)}>Remove</button> 
                 : null
               }
             </div>
           ))}
           <div className="button-section">
-              <button className="button add" type="button" onClick={() => addFormFields()}>Add</button>
-              <button className="button_submit" type="submit">Submit</button>
+              <button className="button add" type="button" onClick={() => addInfoFields()}>Add</button>   
           </div>
+          <div>
+              <h2>Add fields:</h2>
+              <h3>Possible field types: free-text, dropdown menu, numeric input, date and true/false (checkboxes,radio buttons)</h3>
+
+          </div>
+          <div className="button-section">
+              <button className="button add" type="button" onClick={() => addDataFields()}>Add</button>   
+          </div>
+          <button className="button_submit" type="submit">Submit</button>
       </form>
     )
 }
---
+++ src/Components/SchemaComps/DataMethods.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const addRequestedDataField=()=>{
+    return {
+        
+    }
+}
+
+export {addRequestedDataField}
